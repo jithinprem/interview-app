30b979f116d92185fa5e5bfb0b5d26632e635dee
Jithin <jithinprem43@gmail.com>
2022-08-21 02:42:40 +0530

    added_clip_widget

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -7,6 +7,8 @@ import 'package:interview/question.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:interview/actions/get_no_items.dart';
 
+import '../stored_constant/stored_constant.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
   static String id = 'homepage';
@@ -16,32 +18,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  getval() async{
-    GetQue().get_home_question('cn').then((List l){
+  getval() async {
+    GetQue().get_home_question(circle_avatars[_value]).then((List l) {
       allque = l;
     });
-    setState((){
+    setState(() {
       presentque = allque[0]['question'];
       presentans = allque[0]['answer'];
     });
     return allque;
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    get_total_items().then((ret_count){
-      setState((){
+    get_total_items().then((ret_count) {
+      setState(() {
         totalcount = ret_count;
         print('the total item changed to $totalcount');
       });
     });
     print('the total items is set to $totalcount');
     getval();
-
   }
 
+  int _value = 0;
+  List<String> list_subjects = ['CompNet', 'Pyth', 'Java', 'DBMS', 'OpSys', 'DataSt', 'Cprog'];
+  List<String> circle_avatars = ['cn', 'py', 'j', 'db', 'os', 'ds', 'cp'];
   var ind = 0;
   var _vis = false;
   var txtbut = 'SHOW';
@@ -49,17 +53,18 @@ class _HomePageState extends State<HomePage> {
   var ansfield = false;
   var quefield = false;
   String currsubject = 'cn';
-  var select_que =1;   // later do it by reinforcement learning
+  var select_que = 1; // later do it by reinforcement learning
   String presentque = 'welcome to preparenow';
-  String presentans = 'learn for your interviews, viva\'s with us..\nEnter next to view your questions';
+  String presentans =
+      'learn for your interviews, viva\'s with us..\nEnter next to view your questions';
   List allque = [];
 
   TextEditingController que = TextEditingController();
   TextEditingController ans = TextEditingController();
 
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       theme: ThemeData.dark().copyWith(primaryColor: Colors.lightGreen),
       home: SafeArea(
@@ -73,10 +78,9 @@ class _HomePageState extends State<HomePage> {
                 letterSpacing: 2.4,
                 shadows: <Shadow>[
                   Shadow(
-                    offset: Offset(4.0, 4.0),
-                    blurRadius: 6.0,
-                    color: Colors.black54
-                  ),
+                      offset: Offset(4.0, 4.0),
+                      blurRadius: 6.0,
+                      color: Colors.black54),
                 ],
               ),
             ),
@@ -84,15 +88,15 @@ class _HomePageState extends State<HomePage> {
             shadowColor: Colors.lime,
             leading: IconButton(
               icon: Icon(Icons.delete),
-              onPressed: (){
+              onPressed: () {
                 // delete
                 print('this question is going to be deleted');
                 print(allque[select_que]['id']);
                 print(allque[select_que]['question']);
                 int deleteque = allque[select_que]['id'];
-                setState((){
+                setState(() {
                   print('setstate called');
-                  select_que = (select_que + 1)%(totalcount-2);
+                  select_que = (select_que + 1) % (totalcount - 2);
                   presentque = allque[select_que]['question'];
                   presentans = allque[select_que]['answer'];
                 });
@@ -101,14 +105,16 @@ class _HomePageState extends State<HomePage> {
               },
             ),
             actions: [
-              IconButton(onPressed: (){
-                //set all the items to invisible and show text field
-                setState((){
-                  ansfield = !ansfield;
-                  quefield= !quefield;
-                  buttonfield = !buttonfield;
-                });
-              }, icon: Icon(Icons.add)),
+              IconButton(
+                  onPressed: () {
+                    //set all the items to invisible and show text field
+                    setState(() {
+                      ansfield = !ansfield;
+                      quefield = !quefield;
+                      buttonfield = !buttonfield;
+                    });
+                  },
+                  icon: Icon(Icons.add)),
             ],
           ),
           body: Container(
@@ -117,6 +123,31 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                Wrap(
+                  children: List<Widget>.generate(
+                    7,
+                    (int index) {
+                      return ChoiceChip(
+                        avatar: CircleAvatar(
+                          child: Text(circle_avatars[index], style: TextStyle(fontSize: 9),),
+                        ),
+                        selectedColor: Colors.lime[800],
+                        label: Text(list_subjects[index], style: TextStyle(color: Colors.black87),),
+                        selected: _value == index,
+                        onSelected: (bool selected) {
+                          setState(() {
+                            _value = selected ? index : 0;
+                            getval();
+                            // set the subject and call addData again
+                          });
+                        },
+                      );
+                    },
+                  ).toList(),
+                ),
                 Expanded(
                   child: ListView(
                     children: <Widget>[
@@ -125,17 +156,18 @@ class _HomePageState extends State<HomePage> {
                       ),
                       Visibility(
                         visible: quefield,
-                          child: TextField(
-                            controller: que,
-                            minLines: 3,
-                            maxLines: 5,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(20)),
-                              ),
-                              hintText: 'new question',
+                        child: TextField(
+                          controller: que,
+                          minLines: 3,
+                          maxLines: 5,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
                             ),
+                            hintText: 'new question',
                           ),
+                        ),
                       ),
                       Visibility(
                         visible: ansfield,
@@ -144,43 +176,49 @@ class _HomePageState extends State<HomePage> {
                           minLines: 27,
                           maxLines: 28,
                           decoration: const InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(20)),
-                              ),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                            ),
                             hintText: 'solution',
                           ),
                         ),
                       ),
                       Visibility(
-                          visible: buttonfield,
-                          child: TextButton(
-                            onPressed: () async{
-                              String q1 = que.text;
-                              String a1 = ans.text;
-                              computerNetwork.add(computerNetwork.length.toString()+ ') ' +q1);
-                              computerNetwork.add(a1);
-                              // add to database call a function
-                              print('adding to database');
-                              AddtoDataBase().addData(q1, a1, currsubject).then((List l) async{
-                                await getval();
-                              });
-
-                            },
-                            child: Container(
-                              margin: EdgeInsets.only(bottom: 50),
-                              height: 40,
-                              decoration: BoxDecoration(
-                                color: Color(0xffF5F0BB),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              width: double.infinity,
-                              child: const Center(
-                                  child: Text(
-                                    'SUBMIT',
-                                    style: TextStyle(color: Colors.black54, fontFamily: 'Rubik', fontWeight: FontWeight.bold),
-                                  )),
+                        visible: buttonfield,
+                        child: TextButton(
+                          onPressed: () async {
+                            String q1 = que.text;
+                            String a1 = ans.text;
+                            computerNetwork.add(
+                                computerNetwork.length.toString() + ') ' + q1);
+                            computerNetwork.add(a1);
+                            // add to database call a function
+                            print('adding to database');
+                            AddtoDataBase()
+                                .addData(q1, a1, currsubject)
+                                .then((List l) async {
+                              await getval();
+                            });
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 50),
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Color(0xffF5F0BB),
+                              borderRadius: BorderRadius.circular(10),
                             ),
+                            width: double.infinity,
+                            child: const Center(
+                                child: Text(
+                              'SUBMIT',
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontFamily: 'Rubik',
+                                  fontWeight: FontWeight.bold),
+                            )),
                           ),
+                        ),
                       ),
                       Text(
                         //currque[0]['question'],
@@ -201,11 +239,10 @@ class _HomePageState extends State<HomePage> {
                       TextButton(
                         onPressed: () {
                           setState(() {
-                            if(txtbut == 'SHOW'){
+                            if (txtbut == 'SHOW') {
                               _vis = true;
                               txtbut = 'HIDE';
-                            }
-                            else if(txtbut == 'HIDE'){
+                            } else if (txtbut == 'HIDE') {
                               _vis = false;
                               txtbut = 'SHOW';
                             }
@@ -226,9 +263,9 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       TextButton(
-                        onPressed: () async{
-                          setState((){
-                            select_que = (select_que +1)%(allque.length+1);
+                        onPressed: () async {
+                          setState(() {
+                            select_que = (select_que + 1) % (allque.length + 1);
                             presentque = allque[select_que]['question'];
                             presentans = allque[select_que]['answer'];
                           });
@@ -242,16 +279,18 @@ class _HomePageState extends State<HomePage> {
                           width: double.infinity,
                           child: const Center(
                               child: Text(
-                                'NEXT',
-                                style: TextStyle(color: Colors.black54, fontFamily: 'Rubik', fontWeight: FontWeight.bold),
-                              )),
+                            'NEXT',
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontFamily: 'Rubik',
+                                fontWeight: FontWeight.bold),
+                          )),
                         ),
                       ),
-
                       Visibility(
                         visible: false,
                         child: TextButton(
-                          onPressed: () async{
+                          onPressed: () async {
                             AddMannualData().AddData();
                           },
                           child: Container(
@@ -262,14 +301,16 @@ class _HomePageState extends State<HomePage> {
                             ),
                             child: Center(
                                 child: Text(
-                                  'Fetch question',
-                                  style: TextStyle(color: Colors.black54, fontFamily: 'Rubik', fontWeight: FontWeight.bold),
-                                )),
+                              'Fetch question',
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontFamily: 'Rubik',
+                                  fontWeight: FontWeight.bold),
+                            )),
                             width: double.infinity,
                           ),
                         ),
                       ),
-
                     ],
                   ),
                 ),
---
+++ lib/stored_constant/stored_constant.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+List<Widget> chips = [
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('CN'),
+    ),
+    label: const Text('CompNet'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('PY'),
+    ),
+    label: const Text('Python'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('J'),
+    ),
+    label: const Text('Java'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('DB'),
+    ),
+    label: const Text('DBMS'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('OS'),
+    ),
+    label: const Text('OperatingSy'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('D'),
+    ),
+    label: const Text('DataSt'),
+  ),
+  Chip(
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade800,
+      child: const Text('C'),
+    ),
+    label: const Text('Cprog'),
+  ),
+];
